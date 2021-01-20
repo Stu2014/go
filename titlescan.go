6283cc7f0fb6eeb791cc7bb7c45a87cb2912c386
Stu <50605146+Stu2014@users.noreply.github.com>
2021-01-20 17:19:49 +0800

    Update titlescan.go
    增加输出csv文件

--- titlescan.go
+++ titlescan.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"regexp"
+	"strconv"
 	"sync"
+	"time"
 )
 
 /*
@@ -36,6 +41,7 @@ func scnafile(file string, url chan string)  {
 }
 
 func reurl(url chan string)  {
+	timeUnix := time.Now().Format("2006-01-02 15:04:05")+".csv"
 	for{
 		select {
 		case xdd, ok := <-url:
@@ -47,16 +53,42 @@ func reurl(url chan string)  {
 		url := xdd
 		req, err := http.NewRequest("GET", url, nil)
 		if err != nil {
-			panic(err)
+			fmt.Println("url2 error")
+			continue
 		}
 		resp, err := client.Do(req)
-
+		if err != nil {
+			//fmt.Printf("url: %s error\n", url)
+			continue
+		}
 		defer resp.Body.Close()
-		fmt.Printf("[+%d] %s %s \n",resp.StatusCode, xdd, resp.Header.Get("Server"))
+
+		content, _ := ioutil.ReadAll(resp.Body)
+		r := regexp.MustCompile(`<title>(.*)</title>`)
+		ret := r.FindAllStringSubmatch(string(content),-1)
+		//fmt.Println(ret)
+		for _, substr := range ret  {
+			fmt.Printf("[%d+] %s %s %s \n",resp.StatusCode, substr[1], xdd, resp.Header.Get("Server"))
+			writeurl(timeUnix, strconv.Itoa(resp.StatusCode), xdd, substr[1], resp.Header.Get("Server"))
+		}
 		wg.Done()
 		return
 		}
 	}
+}
+
+//写文件
+func writeurl(time_ string, code string, url string, title string, serverver string)  {
+	file , err := os.OpenFile(time_, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	file.WriteString("\xEF\xBB\xBF")
+	if err != nil {
+		fmt.Println("open file err:", err)
+		return
+	}
+	defer file.Close()
+	w := csv.NewWriter(file)
+	w.Write([]string{url, title, code, serverver })
+	w.Flush()
 }
 
 func main() {
@@ -66,7 +98,14 @@ func main() {
 	flag.StringVar(&filename, "f", "1.txt", "请输入url文件名字")
 	flag.Parse()
 	if filename == ""{
-		fmt.Println(`xxx`)
+		fmt.Println(`
+ _____  _  _____  _    ____    ___   ___   __   _   _ 
+(_   _)( )(_   _)( )  (  __)  /  _) (  _) (  ) ( \ ( )
+  | |  | |  | |  | |  | |_    \_"-. | |   /o \ | \\| |
+  ( )  ( )  ( )  ( )_ (  _)    __) )( )_ ( __ )( )\\ )
+  /_\  /_\  /_\  /___\/____\  /___/ /___\/_\/_\/_\ \_\
+                                               v1.0
+`)
 		os.Exit(1)
 	}
 
